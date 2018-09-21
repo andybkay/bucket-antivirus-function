2e5734c8d5c80ba4be7744cbbcedbedf7bbcdb8b
andybkay <evilweasil@me.com>
2018-09-21 13:59:49 +0100

    Update build_lambda.sh

--- build_lambda.sh
+++ build_lambda.sh
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-lambda_output_file=~/build/lambda.zip
+lambda_output_file=build/lambda.zip
 
 set -e
 
